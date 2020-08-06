fa0a83c33c1d52f8a4e398644b959bce06a91ad9
India Rex <face940@gmail.com>
2020-08-06 15:31:51 +0100

    made some progress to fix test for user can submit post but not quite there yet, moving on due to needing help from team

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
-RSpec.feature 'Signup', type: :feature do
+RSpec.feature 'submit post', type: :feature do
   scenario 'Can submit a post' do
-    visit '/people/sign_in'
-    fill_in 'email', with: 'bob@yahoo.com'
-    fill_in 'password', with: 'password123'
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'person_email', with: 'bob@yahoo.com'
+    fill_in 'person_password', with: 'password123'
+    fill_in 'person_password_confirmation', with: 'password123'
     click_button 'Sign up'
-    expect(page).to have_content("You've successfully signed up with bob@yahoo.com")
+    click_button 'New post'
+    fill_in 'post[message]', with: 'This is a post'
+    click_button 'Create Post'
+    expect(page).to have_content("Post was successfully created.")
   end
 end
